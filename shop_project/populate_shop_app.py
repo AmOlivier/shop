ff08efb360634f502c64edd5153370da24084397
Developers Institute <developersinstitute@MacBook-Air-de-Developers.local>
2018-12-10 17:33:43 +0200

    first commit

---
+++ shop_project/populate_shop_app.py
@@ -0,0 +1,49 @@
+import os
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'shop_project.settings')
+import django
+django.setup()
+
+import random
+from faker import Faker
+from shop_app.models import Client, Product
+
+# CONSIGNE
+# creez 2 fonctions :
+#   1. cree 1000 faux clients
+#   2. cree 50 faux produits
+
+fakegen = Faker()
+
+
+def generate_brand():
+	brands = ['Nike', 'Adidas','Reebook', 'Jordan', 'Geox', 'Balenciaga', 'Timberland', 'Asics', 'Puma','New Balance']
+	index= random.randint(0,9)
+	return brands[index]
+
+def generate_clients():
+	for client in range(0,1000):
+		client = Client.objects.get_or_create(first_name=fakegen.first_name(), last_name=fakegen.last_name(), password=fakegen.password(), email=fakegen.email())[0]
+		
+
+def generate_products():
+	for product in range(0,50):
+		price = random.randint(79,399)
+		brand = generate_brand() 
+		product = Product.objects.get_or_create(name=brand, price=price, description =fakegen.text(max_nb_chars=400))
+		print(product)
+
+			
+
+def populate():
+	generate_products()
+	generate_clients()
+
+
+
+
+
+
+if __name__ == '__main__':
+  print('starting populate...')
+  populate()
+  print('done populating')
---
+++ shop_project/shop_app/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+from shop_app.models import Product , Client
+# Register your models here.
+admin.site.register(Product)
+admin.site.register(Client)
